2596723d48c6ef6ab0b9290a216f2d3dfd2ffb16
Rianne Dela Rosa <mrgdelarosa1@tip.edu.ph>
2024-11-19 21:15:03 +0800

    Initial setup with Express server

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) =>{
+    res.send('Render Deployment Project');
+});
+
+app.listen(port, () => {
+    console.log(`Server running o http://localhost:${port}`);
+});
